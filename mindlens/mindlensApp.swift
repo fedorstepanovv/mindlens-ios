decd77ffc8b61225acb315ee8c9db6d81eebe801
Fedir <fedorstepanov0903@gmail.com>
2026-09-10 16:08:01 +0300

    Initial Commit

---
+++ mindlens/mindlensApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindlensApp.swift
+//  mindlens
+//
+//  Created by Fedir Stepanov on 10/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct mindlensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mindlensTests/mindlensTests.swift
@@ -0,0 +1,17 @@
+//
+//  mindlensTests.swift
+//  mindlensTests
+//
+//  Created by Fedir Stepanov on 10/09/2026.
+//
+
+import Testing
+@testable import mindlens
+
+struct mindlensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
